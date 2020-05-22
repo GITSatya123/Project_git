9cb0435c25bf33193db17d1c86840d3cda964978
chintu <chintu@gmail.com>
2020-05-22 17:06:23 +0530

    initial commit

--- Sachin.java
+++ Sachin.java
@@ -2,9 +2,11 @@ class Sachin
 {
 	public static void main(String[] args) 
 	{
-			int i=42%10;
+			int i=70;
+			int j=30;
+			int k=i+j;
 			
-		//System.out.println((i%10)+""+(j%10));
-		System.out.println(i);
+		
+		System.out.println(k);
 	}
 }
