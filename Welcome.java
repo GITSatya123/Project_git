9e4e395d348f2417990c9bc3271a89eda378dad6
chintu <chintu@gmail.com>
2020-05-23 14:39:12 +0530

    final commit

--- Welcome.java
+++ Welcome.java
@@ -7,7 +7,12 @@ public class Demo
 		System.out.println("Welcome to git");
 	}
 	
-		public static void m(){
+		public static void m1(){
 		System.out.println("M1 method ");
 	}	
+
+	public static void m2(){
+		System.out.println("M2 method ");
+	}	
+
 }
